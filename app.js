acb0719479c4ee8e7f6f7cf0273601207f89c991
Fortune Emmanuel <fortuneemmanuel18@gmail.com>
2026-05-15 21:37:37 +0100

    Updated html and added JS

---
+++ app.js
@@ -0,0 +1,23 @@
+const slides = document.querySelectorAll('.slide');
+let slideIndex = 0;
+let intervalId = null;
+
+document.addEventListener("DOMContentLoaded", initializeSlider);
+
+function initializeSlider() {
+
+}
+
+
+function showSlider() {
+
+}
+
+
+function prevSlide() {
+
+}
+
+function nextSlide() {
+
+}
