d9bd85cac6240fa9c50c7dabe0a8ebe2b29ef2b6
Fortune Emmanuel <fortuneemmanuel18@gmail.com>
2026-05-15 22:37:14 +0100

    updated JS

--- app.js
+++ app.js
@@ -7,13 +7,18 @@ document.addEventListener("DOMContentLoaded", initializeSlider);
 function initializeSlider() {
     if (slides.length > 0) {
         slides[slideIndex].classList.add("displaySlide");
-       intervalId = setInterval(nextSlide, 5000)
+       intervalId = setInterval(nextSlide, 5000);
     }
 
 }
-
-
 function showSlide(index) {
+
+    if (index >= slides.length){
+        slideIndex = 0;
+    } 
+    else if (index < 0) {
+        slideIndex = slides.length - 1;
+    }
 
     slides.forEach(slide => {
         slide.classList.remove("displaySlide");
@@ -23,10 +28,12 @@ function showSlide(index) {
 
 
 function prevSlide() {
-
+    clearInterval(intervalId);
+    slideIndex--;
+    showSlide(slideIndex);
 }
 
 function nextSlide() {
     slideIndex++;
-    showSlide();
+    showSlide(slideIndex);
 }
